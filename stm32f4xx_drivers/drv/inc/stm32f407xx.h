74dfb2d3817ebf436fe2493134a46efa4be54f3c
H-Matus <matusevicius.henrikas@gmail.com>
2024-07-21 21:51:26 +0100

    feat(all): Init commit.

---
+++ stm32f4xx_drivers/drv/inc/stm32f407xx.h
@@ -0,0 +1,34 @@
+/*
+ * stm32f407xx.h
+ *
+ *  Created on: Jul 19, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+/* Includes */
+#include <stdint.h>
+
+/* Base addresses of FLASH and SRAM memories */
+
+/* Base addresses of AHBx and APBx Bus Peripherals */
+
+/* Peripheral base addresses: */
+/* GPIO Peripheral Base addresses */
+
+
+/* SPI Peripheral Base addresses */
+
+
+/* TIM Peripheral Base addresses */
+
+
+/* Peripheral Register Structure Definitions */
+
+
+/* Peripheral Clock Enable and Disable macros (RCC peripheral) */
+
+
+#endif /* INC_STM32F407XX_H_ */
